f9c64761fda6422cf1e2f2c7b71436295080d21c
Patrick Drechsler <socialcoding@pdrechsler.de>
2012-09-09 03:37:40 +0200

    added controller

---
+++ 0_src/RepoSync/RepoSync.GuiGtk/MainController.cs
@@ -0,0 +1,29 @@
+using System;
+using RepoSync.GuiGtk.Widgets;
+
+namespace RepoSync.GuiGtk
+{
+	public class MainController
+	{
+		private ChooseConfigWidget chooseConfigWidget;
+		private RepoTreeViewWidget repoTreeViewWidget;
+		private SyncActionWidget syncActionWidget;
+		private SyncOutputWidget syncOutputWidget;
+
+		public MainController (
+			ChooseConfigWidget chooseConfig,
+			RepoTreeViewWidget repoTreeView,
+		 	SyncActionWidget syncAction,
+		  	SyncOutputWidget syncOutput)
+		{
+			this.chooseConfigWidget = chooseConfig;
+			this.repoTreeViewWidget = repoTreeView;
+			this.syncActionWidget = syncAction;
+			this.syncOutputWidget = syncOutput;
+
+			// Register events
+
+		}
+	}
+}
+
