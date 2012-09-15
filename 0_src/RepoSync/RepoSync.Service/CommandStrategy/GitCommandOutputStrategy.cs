92edc821baf59aaf99b0972aef73433370cd6254
Patrick Drechsler <socialcoding@pdrechsler.de>
2012-09-15 11:52:28 +0200

    refactored output format

--- 0_src/RepoSync/RepoSync.Service/CommandStrategy/GitCommandOutputStrategy.cs
+++ 0_src/RepoSync/RepoSync.Service/CommandStrategy/GitCommandOutputStrategy.cs
@@ -6,24 +6,31 @@ namespace RepoSync.Service
 	{
 		public void Execute (System.Diagnostics.Process proc, ICommandResponse response)
 		{
-			string stdin = proc.StandardInput.ToString();
-			string stderr = proc.StandardError.ReadToEnd();
-			string stdout = proc.StandardOutput.ReadToEnd();
+			var stderr = proc.StandardError.ReadToEnd();
+			var stdout = proc.StandardOutput.ReadToEnd();
 			if (IsGitError(stderr)) {
 				response.Success = false;
-				throw new GitCommandException("FATAL GIT COMMAND Exception." + "stdin: " + stdin + " | stdout: " + stdout + " | stderr: " + stderr);
+				throw new GitCommandException("FATAL GIT COMMAND Exception." + CreateOutput(proc, stdout, stderr));
 			}
 			else {
 				response.Success = true;
-				response.Msg = "stdin: " + stdin + " stdout: " + stdout + " stderr: " + stderr;
+				response.Msg = CreateOutput(proc, stdout, stderr);
 			}
-
 		}
 
 		private bool IsGitError (String stderr)
 		{
 			// We probably have to extend this method in the near futuer, because GIT uses stderr for, well, not errors...
 			return stderr.StartsWith("fatal");
+		}
+
+		private string CreateOutput (System.Diagnostics.Process proc, string stdout, string stderr)
+		{
+			return "stdin: " + Environment.NewLine +
+					"Current working directory: " + proc.StartInfo.WorkingDirectory + " " +  Environment.NewLine +
+					proc.StartInfo.FileName  + " " + proc.StartInfo.Arguments +  Environment.NewLine +
+					" stdout: " + Environment.NewLine + stdout +  Environment.NewLine +
+					" stderr: " + Environment.NewLine + stderr;
 		}
 	}
 }
