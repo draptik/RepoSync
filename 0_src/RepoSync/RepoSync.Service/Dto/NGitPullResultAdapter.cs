c9cbda7351676093d449913bce1fe0af69e97948
Patrick Drechsler <socialcoding@pdrechsler.de>
2012-09-22 10:32:54 +0200

    enhanced output og ngit results

--- 0_src/RepoSync/RepoSync.Service/Dto/NGitPullResultAdapter.cs
+++ 0_src/RepoSync/RepoSync.Service/Dto/NGitPullResultAdapter.cs
@@ -10,6 +10,8 @@ namespace RepoSync.Service
 {
 	class NGitPullResultAdapter : ICommandResponse
 	{
+		private static string newLine = Environment.NewLine + "* PULL > ";
+
 		public NGitPullResultAdapter (PullCommand pullCommand)
 		{
 			var stringWriter = new StringWriter();
@@ -20,25 +22,42 @@ namespace RepoSync.Service
 
 			Success = pullResponse.IsSuccessful();
 
-			// TODO Add NGitCommandOutputStrategy
-			Msg = stringWriter.ToString() + " \n\n.........\n\n " + pullResponse.ToString();
+			Msg = stringWriter.ToString() + newLine + " ......... "+ newLine
+				+ pullResponse.ToString();
+			
+			string fetchedFrom = pullResponse.GetFetchedFrom();
+			Msg += "fetchedFrom: " + fetchedFrom;
+
+			FetchResult fetchResult = pullResponse.GetFetchResult();
+
+			var trackingRefUpdates = fetchResult.GetTrackingRefUpdates ();
+			foreach (var trackingRefUpdate in trackingRefUpdates) {
+				var oldObjectId = trackingRefUpdate.GetOldObjectId ().Name;
+				var newObjectId = trackingRefUpdate.GetNewObjectId ().Name;
+				Msg += "Old object id: " + oldObjectId;
+				Msg += "New object id: " + newObjectId;
+
+				var refResult = trackingRefUpdate.GetResult ();
+				Msg += "refResult: " + refResult;
+			}
+
+
+
+			var msg = fetchResult.GetMessages();
+			if (!string.IsNullOrEmpty(msg)) {
+				Msg += newLine + "FetchResult.GetMessages() : " + msg;
+			}
 		}
 
-		public string Msg {
-			get;
-			set;
+		private string msg;
+		public string Msg 
+		{ 
+			get { return msg; } 
+			set { msg = !string.IsNullOrEmpty (value) ? newLine + value : string.Empty; } 
 		}
 
-		public bool Success {
-			get;
-			set;
-		}
-
-		public GitCommandException Exception {
-			get;
-			set;
-		}
+		public bool Success { get; set;	}
+		public GitCommandException Exception { get; set; }
 	}
-
 }
 
--- 0_src/RepoSync/RepoSync.Service/Dto/NGitPushResultAdapter.cs
+++ 0_src/RepoSync/RepoSync.Service/Dto/NGitPushResultAdapter.cs
@@ -9,20 +9,61 @@ namespace RepoSync.Service
 {
 	class NGitPushResultAdapter : ICommandResponse
 	{
+		private static string newLine = Environment.NewLine + "* PUSH > ";
+
 		public NGitPushResultAdapter (Iterable<PushResult> pushResults)
 		{
-			foreach (var pushResult in pushResults) {
-				var remoteUpdates = pushResult.GetRemoteUpdates ();
-				foreach (var remoteUpdate in remoteUpdates) {
+			try {
+				foreach (var pushResult in pushResults) {
 
-					Msg += remoteUpdate.GetStatus ();
+					Msg += "PushResult......";
+
+					var remoteUpdates = pushResult.GetRemoteUpdates ();
+					foreach (var remoteUpdate in remoteUpdates) {
+
+						var oldObjectId = remoteUpdate.GetTrackingRefUpdate ().GetOldObjectId ().Name;
+						var newObjectId = remoteUpdate.GetTrackingRefUpdate ().GetNewObjectId ().Name;
+						Msg += "Old object id: " + oldObjectId;
+						Msg += "New object id: " + newObjectId;
+
+						var refResult = remoteUpdate.GetTrackingRefUpdate ().GetResult ();
+						Msg += "refResult: " + refResult;
+
+						var status = remoteUpdate.GetStatus ();
+						Msg += "STATUS: " + status;
+
+						UpdateSuccessStatus (status);
+
+						if (remoteUpdate.GetMessage () != null) {
+							Msg += remoteUpdate.GetMessage ();
+						}
+
+					}
 				}
+			} catch (System.Exception ex) {
+				this.Exception = new GitCommandException ("Error NGitPushResultAdapter", ex);
 			}
 		}
 
-		public string Msg {	get; set; }
+		private string msg;
+		public string Msg 
+		{ 
+			get { return msg; } 
+			set { msg = !string.IsNullOrEmpty (value) ? newLine + value : string.Empty; } 
+		}
+
 		public bool Success { get; set; }
+
 		public GitCommandException Exception { get; set; }
+
+		private void UpdateSuccessStatus (RemoteRefUpdate.Status status)
+		{
+			if (status == RemoteRefUpdate.Status.OK || status == RemoteRefUpdate.Status.UP_TO_DATE) {
+				Success = true;
+			} else {
+				Success = false;
+			}
+		}
 	}
 
 }
